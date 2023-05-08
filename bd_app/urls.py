24c6bf4659e216a0043656e9a48f4104e9241919
Franco David <david_franco@outlook.com.ar>
2023-05-08 15:45:58 -0300

    static2

--- bd_app/urls.py
+++ bd_app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .views import * 
 from django.urls import path,include
+from django.conf.urls.static import static
 
 
 
@@ -50,4 +51,4 @@ urlpatterns =[
     path('descripcion_za/',descripcion_za,name='descripcion_za'),
  
 
-]
+]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
