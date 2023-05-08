ed8ef2d9a851dba83c0af0c4e4ea9e54928cc365
Franco David <david_franco@outlook.com.ar>
2023-05-08 16:10:26 -0300

    6

--- bd_proyecto/settings.py
+++ bd_proyecto/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2$%(z@zmk2e)i^#6jr*e-y+slcw@$i@ckr@qz7k1wnw4g#p40!'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG ='RENDER' not in os.environ
